feb39672a6fdea0ee2869dcb87c85418af142002
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-02 16:38:19 +0200

    first commit

---
+++ jscript/script.js
@@ -0,0 +1,17 @@
+console.log('JS OK')
+
+/* 
+Passaggi del codice
+
+*/
+
+// Fase preparatoria
+
+// Fase di recupero dati
+
+  // Fase di elaborazione dati
+ 
+  // Fase di restituzione dati
+  
+
+
